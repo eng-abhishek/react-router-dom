257d032b0d6479afb541d03a60733da690175386
Abhishek Soni <soni.abhishek@velocis.in>
2025-10-30 19:02:38 +0530

    manage react routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes,Route,useNavigate } from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Navbar from './Components/Navbar';
 
 function App() {
+
+  const navigator = useNavigate();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <Navbar/>
+   <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/about' element={<About/>}/>
+    <Route path='/contact' element={<Contact/>}/>
+   </Routes>
+   {/*  How to add button click rediraction  */}
+   <button type='button' onClick={() => navigator('/about')}>About Us</button>
+   <br></br>
+   <button type='button' onClick={() => navigator('/contact')}>Contact</button>
+   </>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,35 @@
+import React from "react";
+// import {Link} from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+
+// Link not provide any active class
+// NavLink Provide one active class
+
+function Navbar(){
+      
+ const NavLinkCSS = ({isActive}) => {
+
+    return {
+        fontWeight : isActive ? 'bold' : 'normal',
+        fontSize : isActive ? '15px' : '14px',
+    }
+
+ }
+
+    return(<>
+       <nav>
+        {/* <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link> */}
+
+        {/* NavLink Provide active class */}
+
+        <NavLink to='/' style={NavLinkCSS}>Home</NavLink>
+        <NavLink to='/about' style={NavLinkCSS}>About</NavLink>
+        <NavLink to='/contact' style={NavLinkCSS}>Contact</NavLink>
+
+       </nav>
+    </>);
+}
+
+export default Navbar;
