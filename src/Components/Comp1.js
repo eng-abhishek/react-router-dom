4d8c66acc46f8f52a64feb3041ae516588902a04
abhishek-soni <abhishek-soni@patterndrive.org>
2025-11-01 12:56:33 +0530

    manage code

---
+++ src/Components/Comp1.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Comp1(){
+
+    return(<>
+    <div>
+        <h3>Comp1 Page</h3>
+    </div>
+    </>);
+}
+
+export default Comp1;
---
+++ src/Components/Comp2.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Comp2(){
+
+    return(<>
+    <div>
+        <h3>Comp2 Page</h3>
+    </div>
+    </>);
+    
+}
+
+export default Comp2;
